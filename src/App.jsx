e00fbdc7124844941bb757c5d7c9e9137a8e4c82
Hayderperp94 <boyamerican404@gmail.com>
2024-02-11 23:47:55 +0300

    up

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './Components/Navbar/Navbar'
+import Footer from './Components/Footer/Footer'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -10,6 +11,7 @@ function App() {
   return (
     <div className='app-all'>
     <Navbar/>
+    <Footer/>
     </div>
   )
 }

9f5378b9ba927bed706a1280db71583d0c83ffe0
Hayderperp94 <boyamerican404@gmail.com>
2024-02-12 17:26:01 +0300

    update

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,11 +1,38 @@
 import React from "react";
 import './Footer.css'
+import img from '../../assets/logo.png'
+import facebook from '../../assets/facebook.png'
+import instagram from '../../assets/instagram.png'
+import whatsapp from '../../assets/whatsapp.png'
 
 const Footer =()=>{
     return (
-        <div>
-            <h1>fsfs</h1>
-
+        <div className="footer-all">
+        <div className="mx-5 d-flex justify-content-between align-items-center">
+            
+            <div>
+                <img src={img} alt="" width={"100px"}/>
+            </div>
+            <div>
+                <h1 className="fs-6 mt-5 pt-3">Developed by <span className="text-primary fs-3 fw-bold ">Hayder A. Abdullah</span></h1>
+            </div>
+           
+            <div className="d-flex gap-3 mt-1">
+            <div>
+                <img className="footer-img" src={facebook} alt=""  width={"50px"}/>
+            </div>
+            
+            <div>
+                <img className="footer-img" src={instagram} alt=""  width={"50px"}/>
+            </div>
+            
+            <div>
+                <img className="footer-img" src={whatsapp} alt=""  width={"50px"}/>
+            </div>
+            
+            </div>
+        
+        </div>
         </div>
     )
 }
